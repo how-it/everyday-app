92fc3795cb6d0df1c3450e97e9fe2f4fb7ab49ea
Lovely-Pig <1607806186@qq.com>
2022-01-22 16:59:42 +0800

    chore: add `base` and `server` fields
    Add `base: ./`. Add `port: 8000` and tianapi in proxy.

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,16 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  base: './',
+  plugins: [vue()],
+  server: {
+    port: 8000,
+    proxy: {
+      '/tianapi': {
+        target: 'http://api.tianapi.com',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
