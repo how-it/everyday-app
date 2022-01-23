d0d81529d1959b1f948fff4773ecc08e07f7958f
Lovely-Pig <1607806186@qq.com>
2022-01-23 13:12:21 +0800

    style(main.js): format code with prettier

--- src/main/main.js
+++ src/main/main.js
@@ -1,8 +1,8 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
